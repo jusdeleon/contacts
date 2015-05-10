900af6239e6e6198d33911d28e556cd8d22f8df6
Justin de Leon <jusmdeleon@gmail.com>
2015-05-10 22:48:04 +0800

    Add new contact functionality

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,26 +1,38 @@
 var app = angular.module('contactsApp', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/contacts', {
-		'templateUrl': 'src/views/list.html',
-		'controller': 'ContactsController'
-	});
+	$routeProvider
+        .when('/contacts', {
+    		'templateUrl': 'src/views/list.html',
+    		'controller': 'ContactsController'
+    	})
+        .when('/contacts/new', {
+            'templateUrl': 'src/views/new.html',
+            'controller': 'NewContactController'
+        });
 }]);
 
-app.controller('ContactsController', function ($scope, $http) {
-    $http.get('api/contacts').success(function(response) {
-        $scope.contacts = response;
+app
+    .controller('ContactsController', function ($scope, $http) {
+        $http.get('api/contacts').success(function(response) {
+            $scope.contacts = response;
+        });
+
+        $scope.order = 'first_name';
+        $scope.reverse = false;
+
+        $scope.setOrder = function(order) {
+            $scope.order = order;
+        };
+
+        $scope.setSort = function()
+        {
+            $scope.reverse = ! $scope.reverse;
+        };
+    })
+    .controller('NewContactController', function($scope, $http) {
+        $scope.order = 'first_name';
+        $scope.save = function (contact) {
+            $http.post('api/contacts', contact);
+        }
     });
-
-    $scope.order = 'first_name';
-    $scope.reverse = false;
-
-    $scope.setOrder = function(order) {
-        $scope.order = order;
-    };
-
-    $scope.setSort = function()
-    {
-        $scope.reverse = ! $scope.reverse;
-    };
-});
