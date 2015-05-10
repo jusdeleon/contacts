09c52ae098d8beeecc33a793c51880c75cb6b5e4
Justin de Leon <jusmdeleon@gmail.com>
2015-05-10 13:32:39 +0800

    Integrate AngularJS

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 use Exception;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\File\File;
 
 class Handler extends ExceptionHandler {
 
@@ -36,6 +37,11 @@ class Handler extends ExceptionHandler {
      */
     public function render($request, Exception $e)
     {
+        if($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException)
+        {
+            return file_get_contents('angular.html');
+        }
+
         return parent::render($request, $e);
     }
 
